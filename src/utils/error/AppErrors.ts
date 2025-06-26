2b5beb2f45f3edfe1273dc815f4f7793ff96898c
riazahmedshah <riyazsh360@gmail.com>
2025-06-26 18:40:44 +0530

    added error handler dile

---
+++ src/utils/error/AppErrors.ts
@@ -0,0 +1,13 @@
+import {StatusCodes} from "http-status-codes"
+
+
+export class AppError{
+    message: string;
+    details?:string;
+    statusCode: number
+    constructor(message:string, details?:string, statusCode = StatusCodes.INTERNAL_SERVER_ERROR){
+        this.message = message,
+        this.details = details,
+        this.statusCode = statusCode
+    }
+}

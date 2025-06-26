fe899c84b02324dbe6853791f153ba8aa14bb056
riazahmedshah <riyazsh360@gmail.com>
2025-06-26 20:48:04 +0530

    error file

---
+++ src/utils/error/ServiceError.ts
@@ -0,0 +1,16 @@
+import {StatusCodes} from "http-status-codes"
+
+
+export class ServiceError extends Error{
+    name:string;
+    message: string;
+    details?:string;
+    statusCode: number
+    constructor(name:string, message:string, details?:string, statusCode = StatusCodes.INTERNAL_SERVER_ERROR){
+        super();
+        this.name = name
+        this.message = message,
+        this.details = details,
+        this.statusCode = statusCode
+    }
+}
--- src/utils/error/ValidationError.ts
+++ src/utils/error/ValidationError.ts
@@ -3,13 +3,14 @@ import { StatusCodes } from "http-status-codes";
 export class ValidationError extends Error{
     name:string;
     message:string;
-    details?:string;
+    details?:any;
     statusCode:number;
-    constructor(statusCode:StatusCodes.BAD_REQUEST, details?:any,){
+    constructor(details?:any){
         super();
+
         this.name = "Validation Error";
         this.message = "Validation Failed";
-        this.statusCode = statusCode;
+        this.statusCode = StatusCodes.BAD_REQUEST;
         this.details = details;
     }
 

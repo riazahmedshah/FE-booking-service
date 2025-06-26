b91d0f40bea4e1771a6323573b7b1def3257f041
riazahmedshah <riyazsh360@gmail.com>
2025-06-26 18:18:14 +0530

    first commit

---
+++ prisma/migrations/20250626124624_initialize/migration.sql
@@ -0,0 +1,6 @@
+-- CreateTable
+CREATE TABLE "booking" (
+    "id" SERIAL NOT NULL,
+
+    CONSTRAINT "booking_pkey" PRIMARY KEY ("id")
+);
---
+++ src/config/server.ts
@@ -0,0 +1,5 @@
+import {app} from "../index"
+
+app.listen(1300,() => {
+    console.log("Server is started on PORT 1300")
+})

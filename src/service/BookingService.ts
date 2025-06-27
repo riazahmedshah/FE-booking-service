94f2f2bef6d8e5e64c1136028403195484322f6f
riazahmedshah <riyazsh360@gmail.com>
2025-06-27 15:37:31 +0530

    booking service

--- src/service/BookingService.ts
+++ src/service/BookingService.ts
@@ -1,16 +1,20 @@
 import axios from "axios";
 import { BookingProps } from "../types/bookingTypes";
 import { ServiceError } from "../utils/error/ServiceError";
-import { BookingRepository } from "../repositories/BookingRepository";
+// import { BookingRepository } from "../repositories/BookingRepository";
 
 export class BookingService{
-    static async createBooking(data:BookingProps){
+    static async createBooking(data:BookingProps): Promise<void>{
         try {
-            const response = await axios.get(`http://localhost:1200/flight/${data.flightId}`);
-            const flightData = response.data;
-            console.log(flightData);
-            // await BookingRepository.createBooking(data);
+            const response = await axios.get(`http://localhost:1100/api/v1/flight/${data.flightId}`);
+            const flightData = response.data.flight;
+            if(data.noOfSeats && data.noOfSeats > flightData.totalSeats){
+                throw new ServiceError("Something went wrong in service layer", "Insufficient Seats")
+            }
+
+            return await BookingService.createBooking({...data, totalCost:flightData.price, noOfSeats: flightData.totalSeats})
         } catch (error) {
+            console.log(error)
             throw new ServiceError("Something went wrong in service layer");
         }
 
